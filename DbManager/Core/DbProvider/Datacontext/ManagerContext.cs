6a8b44a922baacdcf43968f1f8ea454b6b3c6498
Smith Wilson <moskalev99@mail.ru>
2017-11-28 02:05:26 +0300

    Контекст бд.

---
+++ DbManager/Core/DbProvider/Datacontext/ManagerContext.cs
@@ -0,0 +1,22 @@
+﻿using DbManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbManager.Core.DbProvider.Datacontext
+{
+    public class ManagerContext : DbContext
+    {
+        public ManagerContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<RootPassword> Passwords { get; set; }
+
+        public DbSet<Facility> Facilitys { get; set; }
+    }
+}
