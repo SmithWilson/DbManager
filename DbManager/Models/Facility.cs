0644b610565206ce2074c0097f5dc93632796081
Smith Wilson <moskalev99@mail.ru>
2017-11-28 02:05:46 +0300

    Модели бд.

---
+++ DbManager/Models/Facility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbManager.Models
+{
+    public class Facility
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Архивный №
+        /// </summary>
+        public int ArchiveNumber { get; set; }
+
+        /// <summary>
+        /// Наименование объекта
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Заказчик
+        /// </summary>
+        public string Client { get; set; }
+
+        /// <summary>
+        /// № Договора
+        /// </summary>
+        public string Treaty { get; set; }
+
+        /// <summary>
+        /// Серия. этажность
+        /// </summary>
+        public string Series { get; set; }
+
+        /// <summary>
+        /// Заключение, архив №
+        /// </summary>
+        public string Conclusion { get; set; }
+
+        /// <summary>
+        /// Дата сдачи в архив
+        /// </summary>
+        public DateTime Data { get; set; }
+
+        /// <summary>
+        ///  Исполнитель
+        /// </summary>
+        public string Executor { get; set; }
+    }
+}
