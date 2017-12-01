2ccd69646a55e7d45ac9750a13b495739a15e006
Smith Wilson <moskalev99@mail.ru>
2017-12-01 13:29:03 +0300

    Реализация интерфейса ICommand.

---
+++ DbManager/Mvvm/DelegateCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DbManager.Mvvm
+{
+    public class DelegateCommand : ICommand
+    {
+        private Action _action;
+
+        public DelegateCommand(Action action)
+            => _action = action ?? throw new ArgumentNullException(nameof(action));
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+            => true;
+
+        public void Execute(object parameter)
+        {
+            try
+            {
+                _action();
+            }
+            catch (Exception)
+            {
+                Debugger.Break();
+            }
+        }
+    }
+}
---
+++ DbManager/Mvvm/DelegateCommand{T}.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DbManager.Mvvm
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        private Action<T> _action;
+
+        public DelegateCommand(Action<T> action)
+            => _action = action ?? throw new ArgumentNullException(nameof(action));
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+            => true;
+
+        public void Execute(object parameter)
+        {
+            try
+            {
+                _action((T)parameter);
+            }
+            catch (Exception)
+            {
+                Debugger.Break();
+            }
+        }
+    }
+}
