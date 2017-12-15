3855cdb23d3cec5b5f382ddc1cd2e7ead7ab5a09
Smith Wilson <moskalev99@mail.ru>
2017-12-16 01:47:49 +0300

    Бинарная сериализация/десериализация.

---
+++ DbManager/Core/DbProvider/Datacontext/Interfaces/IBinarySerializationService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbManager.Core.DbProvider.Datacontext.Interfaces
+{
+    /// <summary>
+    /// Бинарная сериализация.
+    /// </summary>
+    public interface IBinarySerializationService
+    {
+        /// <summary>
+        /// Сериализация <paramref name="obj"/>.
+        /// </summary>
+        /// <typeparam name="T">Передаваемый параметр.</typeparam>
+        /// <param name="path">Путь.</param>
+        /// <param name="obj">Сериализуемый обькт.</param>
+        /// <returns></returns>
+        Task Serialization<T>(string path, T obj);
+
+        /// <summary>
+        /// Десериализация.
+        /// </summary>
+        /// <typeparam name="T">Передаваемый тип.</typeparam>
+        /// <param name="path">Путь.</param>
+        /// <returns>Обьект.</returns>
+        Task<T> Deserialization<T>(string path);
+    }
+}
---
+++ DbManager/Core/Services/SerializationService/BinarySerializationService.cs
@@ -0,0 +1,66 @@
+﻿using DbManager.Core.DbProvider.Datacontext.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbManager.Core.Services.SerializationService
+{
+    public class BinarySerializationService : IBinarySerializationService
+    {
+        //TODO: Доделать и проверить нормальную работу.
+        public Task<T> Deserialization<T>(string path)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    if (String.IsNullOrWhiteSpace(path))
+                    {
+                        throw new ArgumentNullException(nameof(path));
+                    }
+
+                    using (var fs = new FileStream(path, FileMode.Open))
+                    {
+                        var formatter = new BinaryFormatter();
+
+                        var deserializeObject = (T)formatter.Deserialize(fs);
+
+                        return deserializeObject;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debugger.Break();
+                    return default;
+                }
+            });
+        }
+
+        public async Task Serialization<T>(string path, T obj)
+        {
+            try
+            {
+                if (obj == null)
+                {
+                    throw new ArgumentNullException(nameof(obj));
+                }
+
+                using (var fs = new FileStream(path , FileMode.OpenOrCreate))
+                {
+                    var formatter = new BinaryFormatter();
+                    formatter.Serialize(fs, obj);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debugger.Break();
+                return;
+            }
+        }
+    }
+}
